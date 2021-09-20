06725d1ab43d592a9a96fadf42f443a702b51c41
khalil alsulaimani <alsulaimani.2000@outlook.com>
2021-09-20 16:00:23 +0300

    exercise A fully working

--- Exercise_A/Exercise_A_FXMLController.java
+++ Exercise_A/Exercise_A_FXMLController.java
@@ -5,14 +5,27 @@
  */
 package Exercise_A;
 
+import java.awt.Color;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 /**
  * FXML Controller class
@@ -20,6 +33,8 @@ import javafx.scene.layout.Pane;
  * @author alsul
  */
 public class Exercise_A_FXMLController implements Initializable {
+    
+    private double raduis;
 
     @FXML
     private Pane drawingPane;
@@ -28,16 +43,69 @@ public class Exercise_A_FXMLController implements Initializable {
     @FXML
     private TextField Xpostion;
     @FXML
-    private Slider radius;
+    private ComboBox comboBox;
     @FXML
-    private ComboBox<?> comboBox;
+    private Slider radiusSlider;
+    
+    private Circle circle(double x,double y,double radius){
+        Circle circle = new Circle();
+        try{
+            circle.setCenterX(x);
+            circle.setCenterY(y);
+            circle.setRadius(raduis);
+        }catch (Exception e){
+            System.err.println(e);
+        }
+        return circle;
+    }
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        radiusSlider.valueProperty().addListener(new ChangeListener<Number> (){
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number t, Number t1) {
+                raduis =  radiusSlider.getValue();
+            }
+            
+        }
+        );
+        
+        ObservableList<String> list = FXCollections.observableArrayList("White","Gray","Blue");
+        comboBox.setItems(list);
+        
     }    
+
+    @FXML
+    private void drawCircleOnClick(MouseEvent event) {
+        Circle circle = circle(event.getX(),event.getY(),20);
+        drawingPane.getChildren().add(circle);
+    }
+
+    @FXML
+    private void draw(ActionEvent event) {
+        Circle circle = circle(Double.parseDouble(Xpostion.getText()),Double.parseDouble(YPostion.getText()),raduis);
+        drawingPane.getChildren().add(circle);
+        
+        
+    }
+
+    @FXML
+    private void clear(ActionEvent event) {
+        drawingPane.getChildren().clear();
+    }
+
+    @FXML
+    
+    private void backgroundColor(ActionEvent event) {
+        Paint paint = Paint.valueOf(comboBox.getSelectionModel().getSelectedItem().toString());
+        drawingPane.setBackground(new Background(new BackgroundFill(paint,CornerRadii.EMPTY ,Insets.EMPTY)));
+        
+    }
+       
     
 }
